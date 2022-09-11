2c5815edd23dfb8385358821363805a083c89225
User <ruarcanjo86@hotmail.com>
2022-09-11 12:33:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/listaprodutos/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.listaprodutos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+    }
+}
